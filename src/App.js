8ac670ecd0d16f1d57e005ffecc0c577da471eea
summermong <2514o_o@naver.com>
2023-07-24 18:10:53 +0900

    :sparkles: useRef와 리스트 렌더링 반영

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+
+const dummyList = [
+  {
+    id: 1,
+    author: "황윤",
+    content: "안녕하쇼",
+    emotion: 5,
+    created_date: new Date().getTime(),
+    // ms로 시간을 변형하기
+  },
+  {
+    id: 2,
+    author: "유영석",
+    content: "윤이짱!",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
