69b8f52e41efd2f69f48a62a253fa8cfc4158349
summermong <2514o_o@naver.com>
2023-07-25 14:16:49 +0900

    :sparkles: fetch로 데이터 받아오기

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRef } from "react";
 
-// const dummyList = [
-//   {
-//     id: 1,
-//     author: "황윤",
-//     content: "안녕하쇼",
-//     emotion: 5,
-//     created_date: new Date().getTime(),
-//     // ms로 시간을 변형하기
-//   },
-//   {
-//     id: 2,
-//     author: "유영석",
-//     content: "윤이짱!",
-//     emotion: 3,
-//     created_date: new Date().getTime(),
-//   },
-// ];
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]);
   const dataId = useRef(1); // 1부터 시작하게
+
+  const getData = async () => {
+    const res = await fetch(
+      `https://jsonplaceholder.typicode.com/comments`
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = ({ author, content, emotion }) => {
     const created_date = new Date().getTime();
